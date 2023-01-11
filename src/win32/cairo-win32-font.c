8520ef522168882b602efa6c322592e0fe56689f
Fujii Hironori <Hironori.Fujii@sony.com>
2023-01-12 06:57:51 +0900

    Defining Windows version macros in meson.build
    WINVER and _WIN32_WINNT macros were defined in each source files and
    headers that were including <windows.h>. However, because DirectWrite
    requires new Windows API, some files included <windows.h> without the
    version macros. This inconsistency sometimes caused troubles.
    
    Define the version macros in meson.build.

--- src/win32/cairo-win32-font.c
+++ src/win32/cairo-win32-font.c
@@ -33,15 +33,6 @@
  * Contributor(s):
  */
 
-#define WIN32_LEAN_AND_MEAN
-/* We require Windows 2000 features such as GetGlyphIndices */
-#if !defined(WINVER) || (WINVER < 0x0500)
-# define WINVER 0x0500
-#endif
-#if !defined(_WIN32_WINNT) || (_WIN32_WINNT < 0x0500)
-# define _WIN32_WINNT 0x0500
-#endif
-
 #include "cairoint.h"
 
 #include "cairo-win32-private.h"
